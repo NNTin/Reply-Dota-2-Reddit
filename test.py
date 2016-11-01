5c0b98abffb31bd4313fd8c07c7916aba2bffb22
NNTin <ngoctinnguyen1993@gmail.com>
2016-11-01 20:04:11 +0100

    regex test
    regex on dotabuff and opendota links

--- test.py
+++ test.py
@@ -1,71 +1,46 @@
-from steamapi import getrealtimestats
-from steamapi import getschema, getleaguelisting
-from botcommands.toplivegames import topLiveGamesDict, updateTopLiveGamesDict
-from misc.idnamedict import gameMode
+import re
 
+if True:
+    pattern = '(?P<website>yasp\.co|dotabuff\.com|opendota\.com)\/players\/(?P<playerID>\d{1,9})(\/(?P<tab>\w+)((?P<queryParameters>\S+))?)?'
+    patternHero = '[?&](hero|hero_id)=(?P<heroID>\w+)'
+    patternGameMode = '[?&]game_mode=(?P<gameModeID>\w+)'
 
-import time
+    text = 'http://www.dotabuff.com/players/114560539/matches?hero=meepo\n' \
+            'http://www.dotabuff.com/players/114560539/matches?date=year&game_mode=all_pick&hero=meepo\n' \
+            'http://www.dotabuff.com/players/114560539/matches?date=year&hero=meepo&game_mode=all_pick\n' \
+            'http://www.dotabuff.com/players/114560539/matches?game_mode=all_pick&hero=meepo&date=year\n' \
+            'http://www.dotabuff.com/players/114560539/matches?date=year&game_mode=all_pick\t' \
+            '​https://www.opendota.com/players/114560539/matches\n' \
+            'https://www.opendota.com/players/114560539/matches some text\n' \
+            'https://www.opendota.com/players/114560539/matches\n' \
+            'https://www.opendota.com/players/114560539/matches)\n' \
+            'https://www.opendota.com/players/114560539/matches?hero_id=82 some text\n' \
+            'https://www.opendota.com/players/114560539/matches?hero_id=82\n' \
+            'https://www.opendota.com/players/114560539/matches?hero_id=82\n' \
+            'https://www.opendota.com/players/114560539/matches?hero_id=82)\n' \
+            'https://www.opendota.com/players/114560539/matches?with_hero_id=82&hero_id=82&against_hero_id=106&is_radiant=1&win=1&patch=18&game_mode=22&lobby_type=7&region=3&date=180&limit=\n' \
+            'https://www.opendota.com/players/114560539/matches?hero_id=82&with_hero_id=82&against_hero_id=106&is_radiant=1&win=1&patch=18&game_mode=22&lobby_type=7&region=3&date=180&limit=\n' \
+            'https://www.opendota.com/players/114560539/matches?with_hero_id=82&against_hero_id=106&hero_id=82&is_radiant=1&win=1&patch=18&game_mode=22&lobby_type=7&region=3&date=180&limit=\n' \
+            'https://www.opendota.com/players/114560539/matches\n' \
+            'https://www.yasp.co/players/114560539/matches\n' \
+            'https://www.yasp.co/players/114560539/chat?hero_id=82\n' \
 
-serverSteamIDVar = 90101063958206464
+    counter = 0
+    for m in re.finditer(pattern, text, re.I):
+        counter += 1
+        playerID = m.group('playerID')
+        queryParameters = m.group('queryParameters')
+        website = m.group('website')
+        heroID = None
+        gameModeID = None
 
-getschema.requestGetSchema()
-getleaguelisting.requestGetLeagueListing()
+        if queryParameters != None:
+            n = re.search(patternHero, queryParameters, re.I)
+            if n != None:
+                heroID = n.group('heroID')
+            n = re.search(patternGameMode, queryParameters, re.I)
+            if n != None:
+                gameModeID = n.group('gameModeID')
 
-updateTopLiveGamesDict() #update topLiveGamesDict
-
-#generate these information once
-
-game = topLiveGamesDict[str(serverSteamIDVar)]
-
-
-averageMMROrLeague = ''
-gameModeDisplay = '%s' %gameMode(game['game_mode'])
-
-if(game.get('league_id', 0) != 0):
-    leagueID = game['league_id']
-    itemdef = getleaguelisting.leagueListingDictionary[game['league_id']]['itemdef']
-    leagueName = getschema.dota2schema['items_game']['items'][str(itemdef)]['name']
-    averageMMROrLeague = 'League: %s' %leagueName
-
-if(game.get('average_mmr', 0) != 0):
-    averageMMR = game['average_mmr']
-    averageMMROrLeague = 'Average MMR: %s' %averageMMR
-
-
-print(averageMMROrLeague)
-print(gameModeDisplay)
-
-matchID = ''
-
-while True:
-    response = getrealtimestats.getRealtimeStats(serverSteamIDVar)
-    if response == {}:
-        time.sleep(5)
-        continue
-
-    if matchID == '':
-        matchID = response['match']['matchid']
-
-    durationM, durationS = divmod(response['match']['game_time'], 60)
-    matchTime = '%02d:%02d' %(durationM, durationS)
-
-    print('%s Game Mode: %s' %(averageMMROrLeague, gameModeDisplay))
-    print('matchid: %s game time: %s' %(matchID, matchTime))
-
-
-
-    for team in response['teams']:
-        teamName = 'abcdefg'
-        if team['team_name'] != '': teamName = team['team_name']
-        print(teamName)
-
-
-
-#message_template = 'https://www.reddit.com/message/compose/?to=' + botName + '&subject=deletion&message={fullname}'
-#delete_link = message_template.format(fullname=my_new_comment.fullname)
-
-
-    #TODO: implement break condition via GetMatchDetails and timestamp (not game_time!)
-
-    time.sleep(5)
+        print('result #%s: website = %s, playerID = %s, heroID = %s, gameMode = %s' %(counter, website, playerID, heroID, gameModeID))
 
